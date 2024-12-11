dc2311378655c1b9e3a46e3ada252adcfafc8f68
ImadBfOrig <ike.bafou@gmail.com>
2024-12-11 14:22:40 +0100

    MAJ  maps_data.py et de WALL.py
    ** WALL.py **:
    - mise a jour de getting_XandY_of_wall : Changement d'affectation des index_à_"type_de_case"
                                             Ajout de "Power_enable" comme case possible
    
    - mise a jour de wall_drawing : Ajout de draw power/health(bonus) potions
    
    ** maps_data ** :
    - mise a jour des maps avec les pieges et les bonus

--- pictures/image_resized.py
+++ pictures/image_resized.py
@@ -1,15 +1,15 @@
 from PIL import Image
 
 # Load the image
-input_image_path = "helth_bar5.jpg"  # Replace with your image file name
-output_image_path = "health5.png"  # Output file name
+input_image_path = r"C:\Users\ikeba\Desktop\ISI M1\S1\Python _ RBI01\Projet_Python\Git_commun\projet_de_python\pictures\potions\potion_power.png"  # Replace with your image file name
+output_image_path = r"C:\Users\ikeba\Desktop\ISI M1\S1\Python _ RBI01\Projet_Python\Git_commun\projet_de_python\pictures\potions\potion_power_30.png"  # Output file name
 
 try:
     # Open the image
     image = Image.open(input_image_path)  # Removed the extra comma
 
     # Resize the image to 30x10 (width x height)
-    resized_image = image.resize((30, 10), Image.Resampling.LANCZOS)
+    resized_image = image.resize((30, 30), Image.Resampling.LANCZOS)
 
     # Save the resized image as a PNG
     resized_image.save(output_image_path, "PNG")
