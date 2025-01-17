f1b78eece99b2adbb063e91b52b159d3eee3bc12
ImadBfOrig <ike.bafou@gmail.com>
2025-01-17 17:24:53 +0100

    Correction game_grill_main
    Correction Player par Player dans fichier game_grill_main

--- game_grille_main.py
+++ game_grille_main.py
@@ -3,7 +3,7 @@ from wall_adding_imad_code import *
 from AFFICHE_GAME_RESAULT import * 
 from Menu_Game import *
 from EVENT import*
-from Player import*
+from PLAYER import*
 from game_variables import *
 
 pygame.init()
